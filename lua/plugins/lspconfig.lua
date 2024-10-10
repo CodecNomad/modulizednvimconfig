e39e5a62f57ecbf410b1cb99bfdf974a540041d7
codec <codec@gmail.com>
2024-10-10 21:04:42 +0200

    Reformats, lsp fixes

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,7 +3,31 @@ return {
     init = function ()
         local lspconfig = require("lspconfig")
 
-        lspconfig.lua_ls.setup{}
+        lspconfig.lua_ls.setup{
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+                        return
+                    end
+                end
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        version = 'LuaJIT'
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                        }
+                    }
+                })
+            end,
+            settings = {
+                Lua = {}
+            }
+        }
+
         lspconfig.ts_ls.setup{}
         lspconfig.pyright.setup{}
         lspconfig.eslint.setup{}
@@ -15,6 +39,15 @@ return {
                 "rust-analyzer"
             },
         }
-        lspconfig.html.setup{}
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+        lspconfig.html.setup{
+            capabilities = capabilities,
+        }
+
+        require'lspconfig'.html.setup {
+            capabilities = capabilities,
+        }
     end
 }
